60e8a4c952c37fcd76a42b5976e9fc25e871c570
raza <razaaly0007@gmail.com>
2022-12-06 15:24:52 +0500

    inital commit

---
+++ App.js
@@ -0,0 +1,11 @@
+ import React from 'react';
+ import { View, Text, Image, ScrollView, TextInput } from 'react-native';
+ import Providers from './navigation';
+
+ const App = () => {
+   return (
+   <Providers />
+     );
+ }
+
+ export default App;
